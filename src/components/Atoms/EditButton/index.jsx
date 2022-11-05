0c34217dfd0d1ea15420c3cbd55fff6a87ee5f7c
xitras7776 <nishitomo0728@gmail.com>
2022-11-05 22:13:26 +0900

    Atoms/EditButtonの作成

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -13,17 +13,29 @@ const EditButton = ({ checked }) => {
 };
 export default EditButton;
 
-const StyledButton = styled.button``;
-
 const HoverCircle = styled.div`
-width:20px;
-height:20px;
-border-radius:50%;
-background-color:${COLOR.LIGHT_GRAY};
-transition:0.2s;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${COLOR.LIGHT_GRAY};
+  transition: 0.2s;
+  opacity: 0;
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  &:hover > ${HoverCircle} {
+    opacity: 0.2;
+  }
 `;
 
 const PencilImg = styled.img`
+  position: absolute;
   width: 15px;
   height: 15px;
 `;
